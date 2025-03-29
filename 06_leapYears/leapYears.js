c206b9b51b7ecddde38de261159da54911759379
gitsomesleep <37907388+gitsomesleep@users.noreply.github.com>
2025-03-29 17:41:59 -0600

    Complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap year must be divisible by four
+    if (year % 4 !== 0) {
+        return false;
+    }
 
+    // if divisible by 100, must also be divisible by 400
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    }
+    
+    return true;
 };
 
 // Do not edit below this line
