e35a150f296a891ffbe427d07735ac70847800c3
gitsomesleep <37907388+gitsomesleep@users.noreply.github.com>
2025-03-29 17:16:09 -0600

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumOfNums = 0;
 
+    // Determine if valid range
+    if (
+        !Number.isInteger(num1) ||
+        !Number.isInteger(num2) ||
+        num1 < 0 ||
+        num2 < 0
+    ) { return 'ERROR'; }
+
+    // Start count from whichever number is smaller
+    if (num1 <= num2) {
+        for (let i = num1; i <= num2; i++) {
+            sumOfNums += i;
+        }
+    }
+    else {
+        for (let i = num2; i <= num1; i++) {
+            sumOfNums += i;
+        }
+    }
+
+    return sumOfNums;
 };
 
 // Do not edit below this line
