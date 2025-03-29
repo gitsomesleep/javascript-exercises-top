51b0619bc25d93104243cfa525bb9e15326a6ecd
gitsomesleep <37907388+gitsomesleep@users.noreply.github.com>
2025-03-29 01:44:13 -0600

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+
+    function passesTheFilter(item) {
+        for (arg of theArgs) {
+            if (arg === item)
+                return false;
+        }
+        return true;
+    }
+
+    result = arr.filter(passesTheFilter);
+
+    return result;
 };
 
 // Do not edit below this line
