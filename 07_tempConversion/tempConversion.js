a6b125d7d63d5ce59cb8ff8af18382eaac9bad31
gitsomesleep <37907388+gitsomesleep@users.noreply.github.com>
2025-03-29 18:06:40 -0600

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+
+  let degreesCelsius = ( degreesF - 32 ) * (5/9);
+
+  // Round to 10ths place and return as a number rather than a string
+  return parseFloat(degreesCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+
+  let degreesFahrenheit = degreesC * (9/5) + 32;
+
+  // Round to 10ths place and return as a number rather than a string
+  return parseFloat(degreesFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
